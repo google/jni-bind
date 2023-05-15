1dfde756c3bf3c958ac68abb614cad3a48e5f1e2
Jamieson Pryor <jwhpryor@google.com>
2023-05-15 11:01:06 -0700

    Promotion mechanics for `GlobalClassLoader` is now the same as `GlobalObject` (for consistency).
    In a downstream CL this implementation will be merged.
    
    This CL is breaking for any `GlobalClassLoader` constructing from only jobject (switch to `PromoteToGlobal` for identical behaviour).,
    
    PiperOrigin-RevId: 532164374

--- implementation/global_class_loader.h
+++ implementation/global_class_loader.h
@@ -21,6 +21,7 @@
 #include "implementation/class_loader_ref.h"
 #include "implementation/jni_helper/lifecycle_object.h"
 #include "implementation/jvm.h"
+#include "implementation/promotion_mechanics.h"
 #include "jni_dep.h"
 
 namespace jni {
@@ -31,7 +32,13 @@ class GlobalClassLoader : public ClassLoaderRef<jvm_v_, class_loader_v_> {
   using Base = ClassLoaderRef<jvm_v_, class_loader_v_>;
   using LifecycleT = LifecycleHelper<jobject, LifecycleType::GLOBAL>;
 
-  GlobalClassLoader(jobject class_loader)
+  GlobalClassLoader(CreateCopy, jobject class_loader)
+      : Base(static_cast<jobject>(LifecycleT::NewReference(class_loader))) {}
+
+  GlobalClassLoader(AdoptGlobal, jobject class_loader)
+      : ClassLoaderRef<jvm_v_, class_loader_v_>(class_loader) {}
+
+  GlobalClassLoader(PromoteToGlobal, jobject class_loader)
       : ClassLoaderRef<jvm_v_, class_loader_v_>(
             LifecycleT::Promote(class_loader)) {}
 
