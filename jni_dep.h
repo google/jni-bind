c395af746af16fc64fca23e8ecbb37786e1e801f
Jamieson Pryor <jwhpryor@google.com>
2023-08-04 13:05:46 -0700

    No public description
    PiperOrigin-RevId: 553891127

--- jni_dep.h
+++ jni_dep.h
@@ -1,6 +1,8 @@
 #ifndef JNI_BIND_JNI_HELPER_JNI_DEP_H_
 #define JNI_BIND_JNI_HELPER_JNI_DEP_H_
 
+// IWYU pragma: begin_exports
 #include <jni.h>
+// IWYU pragma: end_exports
 
 #endif  // JNI_BIND_JNI_HELPER_JNI_DEP_H_
