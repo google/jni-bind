a62cea5e52ad8192fba5f93a31b2eec4fa3bfddb
Jamieson Pryor <jwhpryor@google.com>
2023-08-07 10:41:25 -0700

    "Forward declare" types to prevent issues with IWYU.
    This CL has no effect.
    
    PiperOrigin-RevId: 554523530

--- jni_dep.h
+++ jni_dep.h
@@ -3,6 +3,41 @@
 
 // IWYU pragma: begin_exports
 #include <jni.h>
+
+// These declarations help prevent bad clang-tidy warnings.
+using jbyte = jbyte;
+using jboolean = jboolean;
+using jchar = jchar;
+using jshort = jshort;
+using jint = jint;
+using jlong = jlong;
+using jfloat = jfloat;
+using jdouble = jdouble;
+using jsize = jsize;
+
+using jobject = jobject;
+using jclass = jclass;
+using jthrowable = jthrowable;
+using jstring = jstring;
+using jarray = jarray;
+using jbooleanArray = jbooleanArray;
+using jbyteArray = jbyteArray;
+using jcharArray = jcharArray;
+using jshortArray = jshortArray;
+using jintArray = jintArray;
+using jlongArray = jlongArray;
+using jfloatArray = jfloatArray;
+using jdoubleArray = jdoubleArray;
+using jobjectArray = jobjectArray;
+
+using jweak = jweak;
+using jvalue = jvalue;
+using jfieldID = jfieldID;
+using jmethodID = jmethodID;
+
+using JavaVM = JavaVM;
+using JNIEnv = JNIEnv;
+
 // IWYU pragma: end_exports
 
 #endif  // JNI_BIND_JNI_HELPER_JNI_DEP_H_
