0b30fd8e7fede8685c08a069d7241b3d23e4bc45
Jamieson Pryor <jwhpryor@google.com>
2026-01-06 13:33:17 -0800

    Remove the requirement to call SetFallbackClassLoader on Android.
    Android, in all its glory, has long had pernicious issues with secondary threads initiating calls on secondary threads without creating a "fallback loader".
    
    This is annoying because it's not intuitive you need to call this, and simply looks like a crash. This is not in the spirit of JNI Bind.
    
    This issue is now resolved by manually scanning for the current activity and scraping the classloader. Because this is done during `JvmRef` init, there is no need to prime the fallback.
    
    PiperOrigin-RevId: 852915505

--- implementation/legacy/jvm_ref_legacy_test.cc
+++ implementation/legacy/jvm_ref_legacy_test.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Google LLC
+ * Copyright 2026 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-#include "implementation/jvm_ref.h"
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
